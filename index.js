61fb2a864f0155baeff8db0b4941ca65e8c0aa92
stephp23 <stephp23@gmail.com>
2021-05-05 01:17:38 -0400

    fixed even number issue

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/ping',(req,res)=> {
+app.get('/', (req, res) => {
+  res.send('This is the root');
+});
+
+app.get('/ping', (req, res) => {
   res.json('pong');
 });
 
@@ -14,7 +18,7 @@ app.get('/greet/:name', (req, res) => {
     const { name } = req.params;
     res.send(`Why hello there, ${name}!`);
   } catch (e) {
-    res.send('Name not found! Please enter a name at end of route.');
+    res.send('Name not found! Please enter a name at end of link.');
   }
 });
 
@@ -26,24 +30,57 @@ app.get('/five', (req, res) => {
 app.get('/evens/:n', async (req, res) => {
   try {
     const { n } = req.params;
-    // nParsedInt = parseInt(n)
+    const nInteger = parseInt(n)
+    // console.log(n)
 
-    if (n > 3) {
-      let evenArr = [];
-      for (let i = 0; i >= n; i += 2) {
-        evenArr.push(i);
+    if (nInteger > 1) {
+      let evenArray = [];
+      for (let i = 2; i <= nInteger; i += 2) {
+        evenArray.push(i);
       }
-      res.send(evenArr);
-      console.log(evenArr)
-  }
-} catch (e) {
-    res.send('Number not found! Please enter number at end of route.')
+      res.send(evenArray);
+      console.log(nInteger)
+    }
+  } catch (e) {
+    // console.log(e);
+    res.send('Number not found! Please enter number at end of link.')
   }
 });
 
-app.get('/fruits',(req,res)=> {
+app.get('/namelength/:name', (req, res) => {
+  try {
+    const { name } = req.params;
+    if (name) {
+      res.send(`The name ${name} is ${name.length} letters long!`);
+    }
+  } catch (e) {
+    res.send('Name not found! Please enter a name at end of link.');
+  }
+});
+
+app.get('/fruits', (req, res) => {
   res.json(fruits);
 });
+
+app.get('/fruits/:name', (req, res) => {
+  try {
+    const { name } = req.params;
+    let fruitNameMatch = fruits.filter((fruit) => {
+      if (fruit.name.toLocaleLowerCase() === name.toLocaleLowerCase()) {
+        return fruit;
+      }
+    });
+    res.json(fruitNameMatch);
+  } catch (e) {
+    res.send('Fruit name not found! Please enter a fruit name at end of link.');
+  }
+});
+
+app.get('/fruits/sort', (req, res) => {
+
+  res.send()
+})
+
 
 app.listen(PORT, () => {
   console.log(`up on port ${PORT}`)
